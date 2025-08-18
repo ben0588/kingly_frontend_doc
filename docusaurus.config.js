2addc087b4d0e4638fab79973b552357ea796dbc
Yenchen <k307849@gmail.com>
2025-08-18 15:49:54 +0800

    add last edit time and fix script command

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -60,10 +60,8 @@ const config = {
       ({
         docs: {
           sidebarPath: "./sidebars.js",
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          showLastUpdateTime: true, // 顯示最後更新時間
+          showLastUpdateAuthor: true, // (可選) 顯示最後更新作者
         },
         blog: {
           showReadingTime: true,
